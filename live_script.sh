9ba9287a6fdfb1b2708fdb99fa68e38897d01853
Fiifi Krampah <fiifi.krampah@gmail.com>
2024-04-28 11:41:40 -0500

    Update live stream publish script

--- live_script.sh
+++ live_script.sh
@@ -1,11 +1,25 @@
-#!/bin/bash 
+#!/bin/bash
+
+# Check if the YouTube live URL argument is provided
 if [ "$1" != "" ]; then
-	echo $1
-    echo "FB URL Provided. Executing script..."
-    fbid=$(echo $1 | sed 's~^.*videos/~~') # delete everything before videos/. output: /id/
-    echo $fbid
-	sed -i "s~videos.*/~videos/$fbid~" Live.html # replace videos/ with videos/id. output: fb video url
-    git add . && git commit -m "Update to GRM Live" && git push origin master
+    echo "YouTube Live URL Provided. Executing script..."
+    # Extract the video ID from the provided URL
+    youtube_id=$(echo $1 | grep -o "live/[a-zA-Z0-9_-]*" | sed 's/live\///')
+
+    # Append any URL parameters, if present
+    url_params=$(echo $1 | grep -o "\?.*$")
+
+    # Check if the video ID was successfully extracted
+    if [ "$youtube_id" != "" ]; then
+        echo "YouTube Video ID: $youtube_id"
+        # Replace the existing YouTube embed URL with the new one in the HTML file
+        # Adding '' for macOS sed compatibility
+        sed -i '' "s~https://www.youtube.com/embed/[a-zA-Z0-9_-]*[?].*~https://www.youtube.com/embed/$youtube_id$url_params~" Live.html
+        # Add the changes to git, commit, and push to the master branch
+        git add Live.html && git commit -m "Update to GRM Live with YouTube video $youtube_id" && git push origin master
+    else
+        echo "Error: Failed to extract YouTube video ID from the URL."
+    fi
 else
-    echo "Please provide the URL of the live FB video like so: ./live_script.sh url"
+    echo "Please provide the full URL of the YouTube live video like so: ./update_live_video.sh 'https://www.youtube.com/live/video_id?si=params'"
 fi
