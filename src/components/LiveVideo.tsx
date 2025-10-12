404edd704612c7ccaa95b57e47035d31cd85db7e
GitHub Action <action@github.com>
2025-10-12 15:23:25 +0000

    Updated live video URL in LiveVideo component

--- src/components/LiveVideo.tsx
+++ src/components/LiveVideo.tsx
@@ -7,7 +7,7 @@ import { Clock, Users, Heart, Play } from 'lucide-react'
 
 export default function LiveVideo() {
   // This URL will be updated by the GitHub Actions workflow
-  const baseVideoUrl = "https://www.youtube.com/embed/E-ZTIZ5k-rU?si=4o0cL6UmrvIApmrY"
+  const baseVideoUrl = "https://www.youtube.com/embed/729tocxsgJg?si=4o0cL6UmrvIApmrY"
   const [showVideo, setShowVideo] = useState(false)
   const [shouldAutoplay, setShouldAutoplay] = useState(false)
 
